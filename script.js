5f89e000c307893d3fff5dcd94b51de75f3c47b3
ThomasTW935 <thomastw935@gmail.com>
2021-01-21 22:02:11 +0800

    Script prints JSON values

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-import data from './data.json'
+init()
 
+function init() {
+  loadJSON((res) => {
+    let data = JSON.parse(res)
+    console.log(data)
+    createHTMLElements(data)
+  })
+}
+function loadJSON(callback) {
+  let xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', './data.json', true)
+  xobj.onreadystatechange = () => {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+      callback(xobj.responseText)
+    }
+  }
+  xobj.send(null)
+}
+function createHTMLElements(data) {
+  let job = document.querySelector('.job')
+  data.forEach((obj) => {
+    // console.log(obj)
+    let listItem = document.createElement('li')
 
+    // Job Logo
+    let imgDiv = document.createElement('div')
+    let img = document.createElement('img')
+    img.setAttribute('src', obj.logo)
+    imgDiv.append(img)
+    listItem.append(imgDiv)
 
-console.log(data)
+    // Job Details
+    createList(listItem, [obj.postedAt, obj.contract, obj.location], 'job__Details')
+
+    // Job Skills
+    createList(listItem, [obj.role, obj.level, ...obj.tools, ...obj.languages], 'job__Skills')
+
+    job.append(listItem)
+  })
+}
+function createList(container, items, className) {
+  let list = document.createElement('ul')
+  list.classList.add(className)
+  items.forEach(item => {
+    let listItem = document.createElement('li')
+    listItem.innerHTML = item
+    list.append(listItem)
+  })
+  container.append(list)
+}
+
